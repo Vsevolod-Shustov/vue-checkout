d4c0226f6d57c54615c4f520837a16dbf8b63325
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2019-11-07 14:16:48 +0300

    20191107

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    Form
+    Form<br>
+    {{cardNumber}}<br>
+    <input v-model='cardNumber' />
+    <button @click="submit">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Form',
-  props: {
-    
+  /* props: {
+    message: String,
+  } */
+  computed: {
+      cardNumber: {
+          get () {
+              return this.$store.state.form.cardNumber
+          },
+          set (cardNumber) {
+              this.$store.commit('updateCardNumber', cardNumber)
+          }
+      }
+  },
+  methods: {
+      updateCardNumber (e) {
+        this.$store.commit('updateCardNumber', e.target.value)
+      },
+      submit () {
+        this.$store.commit('submitTransaction')
+      }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 import Form from './components/Form.vue'
 import History from './components/History.vue'
 
 Vue.config.productionTip = false
 
-
 Vue.use(VueRouter)
+
 const routes = [
   { path: '/', component: Form },
   { path: '/history', component: History }
@@ -15,10 +16,40 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode: 'history'  // short for `routes: routes`
+  mode: 'history'
+})
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  strict: true,
+   state: {
+    form: {
+      cardNumber: '000000000000',
+    },
+    history: [],
+  },
+  mutations: {
+    updateCardNumber (state, cardNumber) {
+      state.form.cardNumber = cardNumber
+    },
+    submitTransaction (state) {
+      state.history.push({'number': state.form.cardNumber})
+    }
+  }
+  /* strict: true,
+    state: {
+        message: 'Hello Vuex',
+    },
+    mutations: {
+        updateMessage (state, message) {
+            state.message = message
+        }
+    } */
 })
 
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
